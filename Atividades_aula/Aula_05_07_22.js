965d62efb19f07aced0c3c7583c4215956ac2ccf
Eduardo V. Lemes <91350022+eduardovlemes@users.noreply.github.com>
2022-07-05 22:04:40 -0300

    feat: executando console.log

---
+++ Atividades_aula/Aula_05_07_22.js
@@ -0,0 +1,13 @@
+//var idade = window.prompt("Digite sua idade:")
+//document.write(idade)
+
+//var isConfirm = window.confirm()
+//document.white(isConfirm)
+//document.white(typeof isConfirm)
+
+
+
+//Exercício do slide 17
+var nome = window.prompt("Digite seu nome:")
+var sobrenome = window.prompt("Digite seu sobrenome:")
+console.log(nome + " " + sobrenome)
